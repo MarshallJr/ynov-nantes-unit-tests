01d04a990e1e2ef11f8d45a6518e275a2f63f211
SpinBoxx <quentin.mimault@orange.fr>
2022-04-21 13:48:29 +0200

    added test for ynov nantes web page

--- 1-intro/workshop/e2e-tests/tests/some_test.js
+++ 1-intro/workshop/e2e-tests/tests/some_test.js
@@ -1,5 +1,13 @@
-Feature('Ynov Nantes');
+Feature('Ynov Nantes feature search bachelor informatique');
 
 Scenario('Test Ynov Nantes Land Page', ({ I }) => {
     I.amOnPage('https://www.ynov-nantes.com/');
+
+    I.click('.extra-cntnr .search-cta .cta');
+
+    I.waitForText('Que recherchez-vous ?', 3);
+
+    I.fillField('.search-form-input .searchfield', 'info');
+
+    I.waitForText('Bachelor Informatique', 3);
 });
