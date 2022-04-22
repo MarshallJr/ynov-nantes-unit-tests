99cf651d82e6085fe2f90219711634b08707e494
Lois <loisviaud@gmail.com>
2022-04-22 15:29:10 +0200

    Feat(Conjured-Test): Add conjured function

--- 2-unit-tests/workshop/js/test/texttest_fixture.js
+++ 2-unit-tests/workshop/js/test/texttest_fixture.js
@@ -8,7 +8,7 @@ const items = [
   new Item("Sulfuras, Hand of Ragnaros", 0, 80),
   new Item("Sulfuras, Hand of Ragnaros", -1, 80),
   new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 11, 10),
   new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
 
   // This Conjured item does not work properly yet
