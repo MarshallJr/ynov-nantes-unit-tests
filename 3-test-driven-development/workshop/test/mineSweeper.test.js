3c08a8cd370f4a354350598cc3e1a3f462133092
lredois <lredois@hipay.com>
2022-04-28 13:38:36 +0200

    [mine-sweeper] refacto:line input

--- 3-test-driven-development/workshop/test/mineSweeper.test.js
+++ 3-test-driven-development/workshop/test/mineSweeper.test.js
@@ -2,57 +2,57 @@ const MineSweeper = require("../src/mineSweeper");
 
 describe("Mine Sweeper", function() {
 
-    // describe("Inputs", function () {
-    //
-    //     it("only one args", function() {
-    //         const input = "4";
-    //         expect(() => {
-    //             new MineSweeper(input);
-    //         }).toThrow();
-    //     });
-    //
-    //     it("more than two args", function() {
-    //         const input = "4 5 6";
-    //         expect(() => {
-    //             new MineSweeper(input);
-    //         }).toThrow();
-    //     });
-    //
-    //     it("two integers", function() {
-    //         const input = "4 5";
-    //         const ms = new MineSweeper(input);
-    //         expect(ms.lines).toBe(4);
-    //         expect(ms.rows).toBe(5);
-    //     });
-    //
-    //     it("not integers", function() {
-    //         const input = "a 5";
-    //         expect(() => {
-    //             new MineSweeper(input);
-    //         }).toThrow();
-    //     });
-    //
-    //     it("0 < n,m < 100", function() {
-    //         const input = "78 97";
-    //         const ms = new MineSweeper(input);
-    //         expect(ms.lines).toBe(78);
-    //         expect(ms.rows).toBe(97);
-    //     });
-    //
-    //     it("one arg under 0", function() {
-    //         const input = "-8 5";
-    //         expect(() => {
-    //             new MineSweeper(input);
-    //         }).toThrow();
-    //     });
-    //
-    //     it("one arg over 100", function() {
-    //         const input = "75 120";
-    //         expect(() => {
-    //             new MineSweeper(input);
-    //         }).toThrow();
-    //     });
-    // });
+    describe("Inputs", function () {
+
+        it("only one args", function() {
+            const input = "4";
+            expect(() => {
+                new MineSweeper(input);
+            }).toThrow();
+        });
+
+        it("more than two args", function() {
+            const input = "4 5 6";
+            expect(() => {
+                new MineSweeper(input);
+            }).toThrow();
+        });
+
+        it("two integers", function() {
+            const input = "4 5";
+            const ms = new MineSweeper(input);
+            expect(ms.rows).toBe(4);
+            expect(ms.columns).toBe(5);
+        });
+
+        it("not integers", function() {
+            const input = "a 5";
+            expect(() => {
+                new MineSweeper(input);
+            }).toThrow();
+        });
+
+        it("0 < n,m < 100", function() {
+            const input = "78 97";
+            const ms = new MineSweeper(input);
+            expect(ms.rows).toBe(78);
+            expect(ms.columns).toBe(97);
+        });
+
+        it("one arg under 0", function() {
+            const input = "-8 5";
+            expect(() => {
+                new MineSweeper(input);
+            }).toThrow();
+        });
+
+        it("one arg over 100", function() {
+            const input = "75 120";
+            expect(() => {
+                new MineSweeper(input);
+            }).toThrow();
+        });
+    });
 
     describe("Field inputs", function () {
 
@@ -97,5 +97,6 @@ describe("Mine Sweeper", function() {
             }).toThrow();
         });
     })
+
 
 });
